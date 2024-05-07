65ead1350090dbadc5ffbf291140b195d0e03725
Biosky <empresaparajesus@gmail.com>
2024-05-07 03:07:04 +0200

    Fix contact mailing

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import "./Contact.css"
 import { motion } from 'framer-motion';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+// import env from "dotenv"
 
 const Contact = () => {
-    const API = "http://localhost:3001/sendmail";
+    // const SERVICE_ID = process.env.EMAILJS_SERVICE_ID
+    // const TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID
+    // const PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY
+    const form = useRef()
+
     const [name, setName] = useState("");
     const [mail, setMail] = useState("");
     const [jobtype, setJobtype] = useState("");
@@ -19,10 +25,7 @@ const Contact = () => {
     const emailValidation = (e) => {
         setMail(e.target.value)
         if (!regEx.test(mail)) {
-
             setErrorMail("Invalid Mail Pattern")
-            toast.error("invalid Email")
-            toast.clearWaitingQueue();
         } else {
             setErrorMail("")
             return true
@@ -33,10 +36,7 @@ const Contact = () => {
     const nameValidation = (e) => {
         setName(e.target.value)
         if (name.length <= 2) {
-
-            setErrorName("Name Should be Atleast 4 Letters")
-            toast.error("Name Should be Atleast 4 Letters")
-            toast.clearWaitingQueue();
+            setErrorName("Name should be atleast 4 Letters")
         } else {
             setErrorName("")
         }
@@ -46,8 +46,6 @@ const Contact = () => {
         if (message.length <= 9) {
 
             setErrorMessage("Text Should be Atleast 10 Letters")
-            toast.error("Text Should be Atleast 10 Letters")
-            toast.clearWaitingQueue();
         } else {
             setErrorMessage("")
         }
@@ -55,39 +53,53 @@ const Contact = () => {
 
     const sendEmail = (event) => {
         event.preventDefault();
-        fetch(API, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            body: JSON.stringify({ name, mail, jobtype, message }),
-        })
-            .then((res) => res.json())
-            .then((result) => {
-                if (result.error) {
-                    toast.error(result.error, {
-                        position: "top-right",
-                        className: "toast-msg"
-                    });
+        if (!name) {
+            return (toast.error("Please enter your name"), toast.clearWaitingQueue())
+        } else if (name.length <= 3) {
+            return (toast.error("Name should be atleast 4 Letters"), toast.clearWaitingQueue())
+        }
 
-                } else {
-                    toast.success(result.success, {
+        if (!mail) {
+            return (toast.error("Please add your E-Mail Address"), toast.clearWaitingQueue());
+        } else if (!regEx.test(mail)) {
+            return (toast.error("Please enter valid Email"), toast.clearWaitingQueue());
+        }
+
+        if (!jobtype) {
+            return (toast.error("Please add Job-type"), toast.clearWaitingQueue());
+        }
+
+        if (!message) {
+            return (toast.error("Please leave a message"), toast.clearWaitingQueue());
+        } else if (message.length <= 9) {
+            return (toast.error("Text should be at least 10 letters"), toast.clearWaitingQueue());
+        }
+
+        emailjs.sendForm("service_zk5h1rp", "template_4fsqz6c", form.current, {
+            publicKey: "uvGjS6myR0OtmZeBm",
+        })
+            .then(
+                () => {
+                    toast.success("Message sent!", {
                         position: "top-right",
-                        className: "toast-msg"
                     });
                     setName("")
                     setMail("")
                     setJobtype("")
                     setMessage("")
-                }
-            })
-            .catch(err => console.log(err));
+                },
+                (error) => {
+                    toast.error(error, {
+                        position: "top-right",
+                        className: "toast-msg"
+                    });
+                },
+            );
+      
     };
 
     return (
         <motion.div initial={{ scale: 0 }} whileInView={{ scale: 1 }} transition={{ type: "spring", stiffness: 30 }} className='vanilla-container1 contact-section' id='contact'>
-
             <div className='row'>
                 <div className='col-xl-5 col-lg-5 col-md-5 col-sm-5'>
                     <div className='contact-form-img'>
@@ -102,12 +114,12 @@ const Contact = () => {
                             <span className='line'></span>
                         </h5>
 
-                        <form>
+                        <form ref={form}>
                             <div className='contact-form'>
                                 <label className='form-label'>{errorName ? <p className='error'>{errorName}</p> : "Name"}</label>
 
                                 <input type='text' style={name.length <= 3 ? { border: "1px solid #fecaca" } : { border: "1px solid #14b8a6" }} className='form-control'
-                                    value={name} onChange={nameValidation}
+                                    name="user_name" value={name} onChange={nameValidation}
                                 />
                             </div>
                             <div className='contact-form'>
@@ -115,13 +127,13 @@ const Contact = () => {
                                 <label className='form-label'>{errorMail ? <p className='error'>{errorMail}</p> : "Email"}</label>
                                 <input type='text' className='form-control'
                                     style={!regEx.test(mail) ? { border: "1px solid #fecaca" } : { border: "1px solid #14b8a6" }}
-                                    value={mail} onChange={emailValidation}
+                                    name="user_email" value={mail} onChange={emailValidation}
                                 />
                             </div>
                             <div className='contact-form'>
                                 <label className='form-label'>Job Type</label>
                                 <select className='custom-select-tag'
-                                    value={jobtype} onChange={e => setJobtype(e.target.value)}
+                                    name="job_type" value={jobtype} onChange={e => setJobtype(e.target.value)}
                                     style={!jobtype ? { border: "1px solid #fecaca" } : { border: "1px solid #14b8a6" }}
                                 >
                                     <option>Full-Time</option>
@@ -133,10 +145,10 @@ const Contact = () => {
                                 <label className='form-label'>{errorMessage ? <p className='error'>{errorMessage}</p> : "Message"}</label>
                                 <textarea type='text' className='form-control'
                                     style={message.length < 10 ? { border: "1px solid #fecaca" } : { border: "1px solid #14b8a6" }}
-                                    value={message} onChange={messageValidation}
+                                    name="message" value={message} onChange={messageValidation}
                                 />
                             </div>
-                            <button onClick={sendEmail} onTouchStart={sendEmail} className='submit'><span>Send <i className="fa-regular fa-paper-plane"></i></span></button>
+                            <button onClick={sendEmail} className='submit'><span>Send <i className="fa-regular fa-paper-plane"></i></span></button>
                         </form>
 
                     </div>
